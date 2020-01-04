14a5c1dba2683aded480ce881ab50564a564727c
Daniel Fehrm <daniel@fehrm.se>
2020-01-04 16:32:17 +0100

    Proper phonenumber validation

--- src/app1/forms.py
+++ src/app1/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from phonenumbers import parse, is_valid_number
+from django.utils.translation import ugettext_lazy as _
+
+
+class PhoneField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        super(PhoneField, self).__init__(*args, **kwargs)
+
+    def clean(self, phonenumber):
+        try:
+            parsed_phone = parse(phonenumber, "SE")
+            if not is_valid_number(parsed_phone):
+                raise forms.ValidationError(_("Not a valid phonenumber"))
+
+            return phonenumber
+        except Exception:
+            raise forms.ValidationError(_("Invalid phonenumber"))
 
 
 # TODO: Create own phonefield
 class PhoneForm(forms.Form):
-    phone_number = forms.CharField(label='Telefonnummer', max_length=100)
+    phone_number = PhoneField(label='Telefonnummer', max_length=100)
