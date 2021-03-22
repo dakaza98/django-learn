067e9eade6887ca175487220d122aa83ef010058
Daniel Fehrm <daniel@fehrm.se>
2021-03-22 13:13:28 +0100

    test django object actions

--- src/app1/admin.py
+++ src/app1/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Question
+from django_object_actions import DjangoObjectActions
 
-admin.site.register(Question)
+
+class QuestionAdmin(DjangoObjectActions, admin.ModelAdmin):
+    def publish_this(modeladmin, request, queryset):
+        self.message_user(request, "We good")
+    
+    def toolfunc(self, request, obj):
+        self.message_user(request, "We goodie")
+    publish_this.label = "Publish"  # optional
+    publish_this.short_description = "Submit this article"  # optional
+
+    change_actions = ('toolfunc', )
+    changelist_actions  = ('publish_this', )
+
+admin.site.register(Question, QuestionAdmin)
